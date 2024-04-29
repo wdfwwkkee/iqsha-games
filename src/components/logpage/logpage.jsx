a1c0cd35589ea3a0a0dee70c19980453dcecc748
H8YOU212 <slavyan.212@mail.ru>
2024-04-29 10:17:52 +0500

    log p

--- src/components/logpage/logpage.jsx
+++ src/components/logpage/logpage.jsx
@@ -6,17 +6,31 @@ import Header from 'Layouts/LayoutsHome/Header'
 
 
 const logpage = () => {
+    const signupButton = 12;
+    const signinButton = 131;
   return (
     <div>
         <Header />
         <div className='Wrapper'>
 
-            <div className='log-container'>
-                s
+            <div className='reg-container'>
+                <form action='#'>
+                    <h1>Sign up</h1> 
+                    <input type="text" placeholder="Name" />
+			        <input type="email" placeholder="Email" />
+			        <input type="password" placeholder="Password" />
+                    <button>Sign Up</button>
+                </form>
             </div>
 
-            <div className='reg-container'>
-
+            <div className='log-container'>
+                    <form action="#">
+                        <h1>Sigh in</h1>
+                        <input type='text' placeholder='Name'/>
+                        <input type="email" placeholder="Email" />
+			            <input type="password" placeholder="Password" />
+                        <button>Sign in</button>
+                    </form>
             </div>
 
         </div>
@@ -25,3 +39,5 @@ const logpage = () => {
 }
 
 export default logpage
+
+// https://codepen.io/FlorinPop17/pen/vPKWjd

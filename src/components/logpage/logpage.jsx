436058f388d0a01eb9b0734654b025da8cf70aae
H8YOU212 <slavyan.212@mail.ru>
2024-04-29 09:44:32 +0500

    sad

---
+++ src/components/logpage/logpage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from 'Layouts/LayoutsHome/Header'
+
+
+
+
+
+const logpage = () => {
+  return (
+    <div>
+        <Header />
+        <div className='Wrapper'>
+
+            <div className='log-container'>
+                s
+            </div>
+
+            <div className='reg-container'>
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default logpage

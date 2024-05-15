8873eb5c2beb5fe2bf6314458207c0414fbe0f2f
H8YOU212 <slava.mikerin@mail.ru>
2024-05-16 00:09:48 +0500

    hp value, accdb to user, hpbar =>Numser

--- src/Layouts/hpbar/hpbar.jsx
+++ src/Layouts/hpbar/hpbar.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NumberSeries from 'components/pages/MathGames/number_series/NumberSeries';
+import { toast } from 'react-toastify';
 
 const hpbar = () => {
-    const starthp = 3;
-    const wronghoice = starthp - 1;
-    const fatalhp = 0;
-    const currenthp = () => {
-      if (starthp === 3) {
-        return (starthp);
-      } else {
-        
+    let starthp;
+    const [hp, setHp] = starthp.useState(3);
+    function hploss() {
+      if (NumberSeries.incorrectAnswer === true) {
+        setHp(prev - 1);
+      }
+      if (hpp === 0) {
+        toast.error('You are dead');
       }
     }
   return (
     <div>
-      <div>your hp</div>
-      <div></div>
+      <div>
+
+        <div>your hp</div>
+
+      </div>
+
     </div>
   )
 }

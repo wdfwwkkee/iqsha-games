09670b175386bb358693164532cc20e48e7325af
H8YOU212 <slavyan.212@mail.ru>
2024-05-21 12:42:09 +0500

    conf

--- src/App.js
+++ src/App.js
@@ -14,16 +14,10 @@ import NumberSeries from 'components/pages/MathGames/number_series/NumberSeries'
 import TaskGames from 'components/pages/MathGames/tasks_games/TaskGames';
 import MoreLess from 'components/pages/MathGames/MoreLess/MoreLess';
 import NumberMathGame from 'components/pages/MathGames/number_game/NumberMathGame';
-<<<<<<< HEAD
 import MathMoneyGame from 'components/pages/MathGames/money_game/MathMoneyGame';
-=======
 import FindCouple from 'components/pages/MemoryGames/Find_a_couple/FindCouple';
 import Find_differences from 'components/pages/MemoryGames/Find_differences/Find_differences';
->>>>>>> 8d999f4b879e247c6ee968ab3b8ccc462310c1f7
-import MathMoneyGame from 'components/pages/MathGames/money_game/MathMoneyGame';
 import Whos_turn from 'components/pages/ChessGames/whos_turn/Whos_turn';
-import FindDifferences from 'components/pages/MemoryGames/Find_differences/Find_differences';
-import FindCouple from 'components/pages/MemoryGames/Find_a_couple/FindCouple';
 
 function App() {
   return (
@@ -46,14 +40,9 @@ function App() {
           <Route element={<TaskGames />} path="/games/math-games/tasks" />
 
 
-
-<<<<<<< HEAD
-          <Route element={<FindCouple />} path="/games/memory-games/find_couple" />
-          <Route element={<FindDifferences />} path="/games/memory-games/find_differences" />
+          
 
     
-=======
->>>>>>> 8d999f4b879e247c6ee968ab3b8ccc462310c1f7
           <Route element={<NumberMathGame />} path="/games/math-games/number" />
           <Route element={<MathMoneyGame />} path="/games/math-games/math_game_money" />
 

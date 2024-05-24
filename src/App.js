222db272f262a7ad425e1fdab30614df0bbb4507
H8YOU212 <slavyan.212@mail.ru>
2024-05-24 13:17:23 +0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Games from 'components/pages/Games/Games';
 // import Home from "./components/pages/Home/Home";
-import Contacts from "./components/pages/Contact/Contacts";
+// import Contacts from "./components/pages/Contact/Contacts";
 
 import 'assets/styles/index.scss'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChessGames from 'components/pages/ChessGames/ChessGames';
 import MathGames from 'components/pages/MathGames/MathGames';
-import ReadingGames from 'components/pages/ReadingGames/ReadingGames';
-import MemoryGames from 'components/pages/MemoryGames/MemoryGames';
+// import ReadingGames from 'components/pages/ReadingGames/ReadingGames';
+// import MemoryGames from 'components/pages/MemoryGames/MemoryGames';
 import LogicGames from 'components/pages/LogicGames/LogicGames';
 import NumberSeries from 'components/pages/MathGames/number_series/NumberSeries';
 import TaskGames from 'components/pages/MathGames/tasks_games/TaskGames';
@@ -26,31 +26,31 @@ function App() {
       <BrowserRouter>
         <Routes>
           {/* GamePages */}
-          <Route element={<Games />} path="/" />
-          <Route element={<ChessGames />} path="/games/chess-games" />
+          <Route exact element={<Games />} path="/iqsha-games/" />
+          <Route element={<ChessGames />} path="/iqsha-games/games/chess-games" />
           <Route element={<MathGames />} path="/games/math-games" />
           {/* <Route element={<ReadingGames />} path="/games/reading-games" /> */}
           {/* <Route element={<MemoryGames />} path="/games/memory-games" /> */}
-          <Route element={<LogicGames />} path="/games/logic-games" />
+          <Route element={<LogicGames />} path="/iqsha-games/games/logic-games" />
 
           {/* MathGames */}
-          <Route element={<MoreLess />} path="/games/math-games/more_lessor" />
-          <Route element={<NumberSeries />} path="/games/math-games/number_series" />
-          <Route element={<TaskGames />} path="/games/math-games/tasks" />         
-          <Route element={<NumberMathGame />} path="/games/math-games/number" />
-          <Route element={<MathMoneyGame />} path="/games/math-games/math_game_money" />
+          <Route element={<MoreLess />} path="/iqsha-games/games/math-games/more_lessor" />
+          <Route element={<NumberSeries />} path="/iqsha-games/games/math-games/number_series" />
+          <Route element={<TaskGames />} path="/iqsha-games/games/math-games/tasks" />         
+          <Route element={<NumberMathGame />} path="/iqsha-games/games/math-games/number" />
+          <Route element={<MathMoneyGame />} path="/iqsha-games/games/math-games/math_game_money" />
 
           {/* <Route element={<Whos_turn />} path="/games/chess-games/whos_turn"/> */}
 
           {/* LogicGames */}
-          <Route element={<WhatExcess />} path="/games/logic-games/what_excess"/>
-          <Route element={<ContinueSeries />} path="/games/logic-games/continue_series"/>
-          <Route element={<OneWord />} path="/games/logic-games/one_word"/>
+          <Route element={<WhatExcess />} path="/iqsha-games/games/logic-games/what_excess"/>
+          <Route element={<ContinueSeries />} path="/iqsha-games/games/logic-games/continue_series"/>
+          <Route element={<OneWord />} path="/iqsha-games/games/logic-games/one_word"/>
 
 
           {/* ChessGames */}
           
-          <Route element={<WhosTurn />} path="/games/chess-games/whos_turn"/>
+          <Route element={<WhosTurn />} path="/iqsha-games/games/chess-games/whos_turn"/>
 
 
 
@@ -61,7 +61,7 @@ function App() {
 
 
           {/* Other */}
-          <Route element={<Contacts />} path="/contacts" />
+          {/* <Route element={<Contacts />} path="/contacts" /> */}
 
           <Route path="/*" element={<div>NotFound</div>} />
           

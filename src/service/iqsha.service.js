a800549256977bef52a0c872eff41bcd10c94b5e
wdfwwkkee <denis.usmanov101@mail.ru>
2024-06-03 21:03:26 +0500

    clea

--- src/service/iqsha.service.js
+++ src/service/iqsha.service.js
@@ -1,19 +1,16 @@
-import { QuerySnapshot, collection, doc, getDoc, getDocs, query } from 'firebase/firestore';
-import { useState } from 'react';
-import { db } from 'utils/firestore';
-
+import { collection, getDocs, query } from "firebase/firestore";
+import { db } from "utils/firestore";
 
 export const iqshaService = {
-    
-    getAllGames : async() => {
-        const q = query(collection(db, "data.sourcePic"));
-        const querySnapshot = await getDocs(q);
-        return querySnapshot.docs[0].data().nameCategory;
-    },
-    getGameById : async(id) => {
-        const q = query(collection(db, "data.sourcePic"));
-        const querySnapshot = await getDocs(q);
-        const array =  querySnapshot.docs[0].data().nameCategory;
-        return array[id-1]
-    }
-}
+  getAllGames: async () => {
+    const q = query(collection(db, "data.sourcePic"));
+    const querySnapshot = await getDocs(q);
+    return querySnapshot.docs[0].data().nameCategory;
+  },
+  getGameById: async (id) => {
+    const q = query(collection(db, "data.sourcePic"));
+    const querySnapshot = await getDocs(q);
+    const array = querySnapshot.docs[0].data().nameCategory;
+    return array[id - 1];
+  },
+};
